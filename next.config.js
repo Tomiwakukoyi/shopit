1a41f5baa7d5fb02f57ea702ef334d0dd2a912c5
Tomiwa Kukoyi <tomiwakukoyiweb@gmail.com>
2023-02-03 01:20:20 +0100

    zz

--- next.config.js
+++ next.config.js
@@ -9,4 +9,11 @@ const nextConfig = {
   },
 };
 
-module.exports = nextConfig;
+module.exports = {
+  nextConfig,
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+}
